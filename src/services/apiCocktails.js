bdf182a459dcbbb82ef884329ab8ec4f8df19d3f
Jeudi Espinoza <jeudiecca@gmail.com>
2023-11-03 01:24:49 -0600

    Change proxy

--- src/services/apiCocktails.js
+++ src/services/apiCocktails.js
@@ -1,6 +1,6 @@
 export async function getCocktails(search) {
   const API_URL = "http://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-  const CORS_API_URL = "https://cors-anywhere.herokuapp.com/";
+  const CORS_API_URL = "https://corsproxy.io/?";
 
   const res = await fetch(CORS_API_URL + API_URL + search);
   const data = await res.json();
